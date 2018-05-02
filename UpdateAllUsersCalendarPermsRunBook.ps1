097656099b2f3a8480919f7e585ea28c9fc176c4
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-02 12:35:56 +0100

    Add files via upload

---
+++ UpdateAllUsersCalendarPermsRunBook.ps1
@@ -0,0 +1,10 @@
+﻿#Must add the ExchangeModule to Azure from the Gallery to run exchange commands in the cloud
+#This example allows all your users to see each others calenders without having to send request
+
+$credObject = Get-AzureAutomationPSCredential -Name "YourAutomationAccountHere"
+Connect-MsolService -Credential $credObject
+Set-ModuleCredential -Cred $credObject -Conn "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.1.14393.187"
+foreach($user in Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize unlimited)
+{ $cal = $user.alias+":Calendar"
+Set-MailboxFolderPermission -Identity $cal -User Default -AccessRights Reviewer
+}
