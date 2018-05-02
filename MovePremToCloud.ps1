386e7bf7a5711476b43f8035588f7cb7c6f2540d
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-02 10:42:25 +0100

    Add files via upload

---
+++ MovePremToCloud.ps1
@@ -0,0 +1,14 @@
+﻿# Included in this folder is a sample move.csv
+# Best used on an On-Prem Exchange server as allPS modules loaded as standard  
+
+
+
+$LiveCred = Get-Credential 
+Import-PSSession $Session 
+$ONPREMCREDS = Get-Credential
+$RemHostName = "Your On-Prem Exchnage Exchange Enviroment"
+$TargDom = "Your Office 365 Tenant"
+$MB = import-csv c:\temp\move.csv
+foreach($ID in $MB)
+{$UserID = $ID.UserID 
+New-MoveRequest -Identity $UserID -Remote -RemoteHostName $RemHostName  -TargetDeliveryDomain $TargDom -RemoteCredential $ONPREMCREDS -BadItemLimit 100 }
