0dc110e9a0a0e5cab1bb721cc886be3d2cc715f4
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-03 11:17:03 +0100

    Add files via upload

---
+++ RestoreSoftdeletedMailbox.ps1
@@ -0,0 +1,7 @@
+﻿$LiveCred = Get-Credential
+Set-ExecutionPolicy Unrestricted 
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $LiveCred -Authentication Basic -AllowRedirection 
+Import-PSSession $Session 
+$SourceMailbox = (Get-Mailbox -SoftDeletedMailbox "User Email").ExchangeGuid 
+$TargetMailbox = (Get-Mailbox -identity "User email").ExchangeGuid 
+New-MailboxRestoreRequest -SourceMailbox $SourceMailbox -TargetMailbox $TargetMailbox  -AllowLegacyDNMismatch -verbose 
