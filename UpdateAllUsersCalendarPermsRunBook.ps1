cad55a9863cafd73083db1f9d0f949d8be944230
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-03 09:59:05 +0100

    Update UpdateAllUsersCalendarPermsRunBook.ps1

--- UpdateAllUsersCalendarPermsRunBook.ps1
+++ UpdateAllUsersCalendarPermsRunBook.ps1
@@ -1,7 +1,7 @@
 ﻿#Must add the ExchangeModule to Azure from the Gallery to run exchange commands in the cloud
 #This example allows all your users to see each others calenders without having to send request
 
-$credObject = Get-AzureAutomationPSCredential -Name "YourAutomationAccountHere"
+$credObject = Get-AzureAutomationPSCredential -Name "YourAutomationCredentials"
 Connect-MsolService -Credential $credObject
 Set-ModuleCredential -Cred $credObject -Conn "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.1.14393.187"
 foreach($user in Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize unlimited)
