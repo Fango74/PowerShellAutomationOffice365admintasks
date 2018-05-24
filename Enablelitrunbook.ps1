e7bd794c43b94bc12fe1bdc019b6f98967f7356e
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-24 09:53:11 +0100

    Add files via upload

---
+++ Enablelitrunbook.ps1
@@ -0,0 +1,8 @@
+﻿$credObject = Get-AzureAutomationPSCredential -Name "YourAutomationAccountHere"
+Connect-MsolService -Credential $credObject
+Set-ModuleCredential -Cred $credObject -Conn "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.1.14393.187"
+get-mailbox  | where-object {$_.litigationholdenabled -eq $false} | Export-CSV "C:\litenabled.csv"
+$litenabled = import-csv c:\litenabled.csv
+{ $lit = $litenabled.alias
+Set-Mailbox $lit -LitigationHoldEnabled $true -LitigationHoldDuration 1000
+}
