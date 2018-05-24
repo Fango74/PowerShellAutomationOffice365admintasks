a944251062661d90d21f6286614d391b590907c8
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-24 09:55:12 +0100

    Update Enablelitrunbook.ps1

--- Enablelitrunbook.ps1
+++ Enablelitrunbook.ps1
@@ -1,4 +1,8 @@
-﻿$credObject = Get-AzureAutomationPSCredential -Name "YourAutomationAccountHere"
+#Runbook for Azure autonmation to find all users without the Litigation hold flag and enabling it.
+#Be careful as i think if you reset it on a mailbox that already has it enabled it my start the hod again loosing all the passed mail
+#Please someone confirm the above
+
+$credObject = Get-AzureAutomationPSCredential -Name "YourAutomationAccountHere"
 Connect-MsolService -Credential $credObject
 Set-ModuleCredential -Cred $credObject -Conn "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.1.14393.187"
 get-mailbox  | where-object {$_.litigationholdenabled -eq $false} | Export-CSV "C:\litenabled.csv"
