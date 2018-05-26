e6623aeb2b393fb409a818d6deb69fd5acfe21bb
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-26 17:51:39 +0100

    Add files via upload

---
+++ UpdatereceiveconnectorwithEOPIPAddresses.ps1
@@ -0,0 +1,6 @@
+﻿$FormatEnumerationLimit =-1
+$O365Cred = Get-Credential YourTenantAdmin@tenant.onmicrosoft.com
+$O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $O365Cred -Authentication Basic -AllowRedirection
+Import-PSSession $O365Session
+$ip = Get-HybridMailflowDatacenterIPs
+Get-ReceiveConnector "Receive Connector Name" | Set-ReceiveConnector -RemoteIPRanges $ip.datacenterips
