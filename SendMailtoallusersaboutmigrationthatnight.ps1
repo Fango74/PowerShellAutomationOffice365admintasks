b4d592bdacce2973d49b56adb1def4719aea7b23
Daniel Nicholls <38655170+Fango74@users.noreply.github.com>
2018-05-28 19:29:50 +0200

    Add files via upload

---
+++ SendMailtoallusersaboutmigrationthatnight.ps1
@@ -0,0 +1,6 @@
+﻿#Send a message to allusesr that they are in the migration batch for tonights move to O365
+#This is per DB but "get-mailbox -Organizationunit" maybe be better andjust move the users you want in/out of it before/after migration
+
+$Boxes = Get-Mailbox -Database "YOURDBNAME"
+Foreach ($Box in $Boxes){Send-MailMessage -Subject "Migration of Mailbox" -Body "Hello, your mailbox will be migrated to the new Exchange server this evening, please remember to shut down your system tonight"
+ -From "admin@yourdomain.com" -To $Box.PrimarySmtpAddress -SmtpServer "Exchangeserver.yourdomain.com"}
